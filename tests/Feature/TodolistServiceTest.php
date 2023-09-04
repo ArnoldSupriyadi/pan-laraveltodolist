de8976049c3dca34c42f20ec2ddb31c1df5c4284
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-09-04 13:01:20 +0700

    60. menambahkan logic to do

---
+++ tests/Feature/TodolistServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\TodolistService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Session;
+use Tests\TestCase;
+
+class TodolistServiceTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    private TodolistService $todolistService;
+
+    protected function setUp():void
+    {
+        parent::setUp();
+
+        $this->todolistService = $this->app->make(TodolistService::class);
+    }
+
+    public function testTodolistNotNull()
+    {
+        self::assertNotNull($this->todolistService);
+    }
+
+    public function testSaveTodo()
+    {
+        $this->todolistService->saveTodo("1", "Eko");
+
+        $todolist = Session::get("todolist");
+        foreach($todolist as $value){
+            self::assertEquals("1", $value['id']);
+            self::assertEquals("Eko", $value['todo']);
+        }
+    }
+    
+}
